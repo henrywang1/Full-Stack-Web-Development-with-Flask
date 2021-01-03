979bd571e19550d156c7f759335181059f241307
Henry <wanghy917@gmail.com>
2021-01-02 18:11:56 -0600

    add route and index

--- application/route.py
+++ application/route.py
@@ -1,6 +1,24 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html", login=True)
+
+@app.route("/login")
+def login():
+    return render_template("index.html", login=False)
+
+@app.route("/register")
+def register():
+    return render_template("index.html", login=False)
+
+@app.route("/courses")
+def courses():
+    return render_template("index.html", login=False)
+
+@app.route("/enrollment")
+def enrollment():
+    return render_template("index.html", login=False)
